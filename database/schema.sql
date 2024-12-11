a2a7fd32532b99a58ba7da315ca58260ef553d30
Marvin Alegre <marvinalegredev@gmail.com>
2024-12-11 17:35:18 +0800

    add db schema

---
+++ database/schema.sql
@@ -0,0 +1,35 @@
+DROP TABLE IF EXISTS user_questions;
+CREATE TABLE IF NOT EXISTS user_questions (
+    user_id INTEGER NOT NULL UNIQUE,
+    question TEXT NOT NULL,
+    answer TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS user_pokemons;
+CREATE TABLE IF NOT EXISTS user_pokemons (
+    user_id INTEGER NOT NULL,
+    pokemon_id INTEGER NOT NULL,
+    caught_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (pokemon_id) REFERENCES pokemons(id)
+);
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    jwt_id TEXT NOT NULL UNIQUE,
+    username TEXT NOT NULL UNIQUE,
+    hashed_password TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS pokemons;
+CREATE TABLE IF NOT EXISTS pokemons (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    description TEXT NOT NULL UNIQUE,
+    color TEXT NOT NULL,
+    availability TEXT NOT NULL
+);
