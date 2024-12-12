749acd61e6582826eac4d21b8253bd420efa1949
Marvin Alegre <marvinalegredev@gmail.com>
2024-12-12 10:33:06 +0800

    add home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,19 +1,30 @@
 import type { Route } from "./+types/home";
-import { useLoaderData } from "react-router";
+import { Link, useLoaderData } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "PokéMath" }];
 }
 
-export async function clientLoader() {
-  const res = await fetch("/api/hw");
-  const { message } = await res.json();
-
-  return { message };
-}
-
 export default function Home() {
-  const { message } = useLoaderData();
-
-  return <p>{message}</p>;
+  return (
+    <>
+      <nav className="flex justify-between items-center py-2 px-1 relative h-9 text-black bg-gray-400">
+        <Link to="/">
+          <div className="font-semibold text-3xl italic">PokéMath</div>
+        </Link>
+        <ul className="flex text-gray-300 space-x-8 ml-10 text-xl">
+          <li>
+            <button className="py-1 text-black">menu</button>
+          </li>
+        </ul>
+      </nav>
+      <div className="flex items-center justify-center h-[calc(100vh-2.25rem)] md:h-[70vh]">
+        <Link to="catch">
+          <div className="pokeball">
+            <div className="pokeball-button"></div>
+          </div>
+        </Link>
+      </div>
+    </>
+  );
 }
