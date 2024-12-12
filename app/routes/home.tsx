66cdaf67a9d4958e5b13ee6ed30bad4fb9bd25d5
Marvin Alegre <marvinalegredev@gmail.com>
2024-12-12 11:20:16 +0800

    add menu button

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,11 +1,18 @@
 import type { Route } from "./+types/home";
 import { Link, useLoaderData } from "react-router";
+import { useState } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "PokéMath" }];
 }
 
 export default function Home() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  function handleMenuClick() {
+    setShowMenu(!showMenu);
+  }
+
   return (
     <>
       <nav className="flex justify-between items-center py-2 px-1 relative h-9 text-black bg-gray-400">
@@ -14,17 +21,36 @@ export default function Home() {
         </Link>
         <ul className="flex text-gray-300 space-x-8 ml-10 text-xl">
           <li>
-            <button className="py-1 text-black">menu</button>
+            <button onClick={handleMenuClick} className="py-1 text-black">
+              {showMenu ? "close menu" : "menu"}
+            </button>
           </li>
         </ul>
       </nav>
-      <div className="flex items-center justify-center h-[calc(100vh-2.25rem)] md:h-[70vh]">
-        <Link to="catch">
-          <div className="pokeball">
-            <div className="pokeball-button"></div>
-          </div>
-        </Link>
-      </div>
+      {!showMenu && (
+        <div className="flex items-center justify-center h-[calc(100vh-2.25rem)] md:h-[70vh]">
+          <Link to="catch">
+            <div className="pokeball">
+              <div className="pokeball-button"></div>
+            </div>
+          </Link>
+        </div>
+      )}
+      {showMenu && (
+        <ul className="text-xl">
+          <MenuLink link="marvinalegre" to="marvinalegre" />{" "}
+          <MenuLink link="players" to="players" />{" "}
+          <MenuLink link="log out" to="logout" />
+        </ul>
+      )}
     </>
+  );
+}
+
+function MenuLink({ link, to }) {
+  return (
+    <Link to={to}>
+      <li className="hover:bg-white p-2">{link}</li>
+    </Link>
   );
 }
