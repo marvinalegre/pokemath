545dd5c7f22fdcb6b5d34f48ffbc0684b199bc66
Marvin Alegre <marvinalegredev@gmail.com>
2024-12-14 15:57:22 +0800

    refactor users page

--- app/routes/user.tsx
+++ app/routes/user.tsx
@@ -6,12 +6,41 @@ export function meta({ params }: Route.MetaArgs) {
   return [{ title: `PokéMath | ${params.username}` }];
 }
 
-export default function User({ params }) {
+export default function User({ params }: Route.ComponentProps) {
   const [showMenu, setShowMenu] = useState(false);
+  const pinnedPokemons = [
+    {
+      pokemonId: 1,
+      userPokemonId: 1,
+    },
+    {
+      pokemonId: 3,
+      userPokemonId: 1,
+    },
+    {
+      pokemonId: 9,
+      userPokemonId: 1,
+    },
+  ];
+  const pokemons = [
+    {
+      pokemonId: 25,
+      userPokemonId: 1,
+    },
+    {
+      pokemonId: 4,
+      userPokemonId: 1,
+    },
+    {
+      pokemonId: 6,
+      userPokemonId: 1,
+    },
+  ];
 
   function handleMenuClick() {
     setShowMenu(!showMenu);
   }
+
   return (
     <>
       <nav className="sticky top-0 flex justify-between items-center py-2 px-1 relative h-9 text-black bg-gray-400">
@@ -30,217 +59,37 @@ export default function User({ params }) {
         <>
           <p className="sticky top-9 bg-white p-2 text-xl">{params.username}</p>
           <div className="flex flex-row flex-wrap justify-center items-center max-w-[1400px] mx-auto gap-2">
-            <img
-              className={`w-40 md:w-48`}
-              src="https://pokemons.pages.dev/sprites/pm0006_00_00_00_big.png"
-            />
-            <img
-              className={`w-40 md:w-48`}
-              src="https://pokemons.pages.dev/sprites/pm0002_00_00_00_big.png"
-            />
-            <img
-              className={`w-40 md:w-48`}
-              src="https://pokemons.pages.dev/sprites/pm0003_00_00_00_big.png"
-            />
-            <img
-              className={`w-40 md:w-48`}
-              src="https://pokemons.pages.dev/sprites/pm0150_00_00_00_big.png"
-            />
-            <img
-              className={`w-40 md:w-48`}
-              src="https://pokemons.pages.dev/sprites/pm0146_00_00_00_big.png"
-            />
-            <img
-              className={`w-40 md:w-48`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
+            {pinnedPokemons.map((p) => (
+              <img
+                className={`w-40 md:w-48`}
+                src={`https://pokemons.pages.dev/sprites/pm${String(
+                  p.pokemonId
+                ).padStart(4, "0")}_00_00_00_big.png`}
+              />
+            ))}
           </div>
           <div className="my-12 md:pb-12 flex flex-row flex-wrap justify-center items-center max-w-[1400px] mx-auto gap-1">
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0003_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0006_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0006_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0003_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0006_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0006_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0003_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0006_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0006_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0003_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0006_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0006_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0003_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0006_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0006_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0003_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0006_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0006_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
-            <img
-              className={`w-28 md:w-32`}
-              src="https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png"
-            />
+            {pokemons.map((p) => (
+              <img
+                className={`w-28 md:w-32`}
+                src={`https://pokemons.pages.dev/sprites/pm${String(
+                  p.pokemonId
+                ).padStart(4, "0")}_00_00_00_big.png`}
+              />
+            ))}
           </div>
         </>
       )}
       {showMenu && (
         <ul className="text-xl">
-          <MenuLink link="players" to="players" />{" "}
-          <MenuLink link="log out" to="logout" />
+          <Link to="/players">
+            <li className="hover:bg-white p-2">players</li>
+          </Link>
+          <Link to="/logout">
+            <li className="hover:bg-white p-2">log out</li>
+          </Link>
         </ul>
       )}
     </>
-  );
-}
-
-function MenuLink({ link, to }) {
-  return (
-    <Link to={to}>
-      <li className="hover:bg-white p-2">{link}</li>
-    </Link>
   );
 }
