10a10384155562c9a50ef788d967bc1cc3869a9d
Marvin Alegre <marvinalegredev@gmail.com>
2024-12-15 12:38:55 +0800

    add new pokemon options and got away message

--- app/routes/catch.tsx
+++ app/routes/catch.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { Route } from "./+types/catch";
 import { Link, Form } from "react-router";
 
@@ -5,7 +6,33 @@ export function meta({}: Route.MetaArgs) {
   return [{ title: "PokéMath | Catch" }];
 }
 
-export default function Login() {
+export default function Catch() {
+  const [showQuestion, setShowQuestion] = useState(true);
+  const [showNewPokemon, setShowNewPokemon] = useState(false);
+  const [showGotAwayMessage, setShowGotAwayMessage] = useState(false);
+
+  function handleSubmit() {
+    const caughtSomething = Math.round(Math.random() * 10) % 2 === 1;
+
+    if (caughtSomething) {
+      setShowQuestion(false);
+      setShowNewPokemon(true);
+    } else {
+      setShowQuestion(false);
+      setShowGotAwayMessage(true);
+    }
+  }
+
+  function handleOptionClick() {
+    setShowQuestion(true);
+    setShowNewPokemon(false);
+  }
+
+  function handleOkClick() {
+    setShowQuestion(true);
+    setShowGotAwayMessage(false);
+  }
+
   return (
     <>
       <nav className="flex justify-between items-center py-2 px-1 relative h-9 text-black bg-gray-400">
@@ -13,32 +40,87 @@ export default function Login() {
           <div className="font-semibold text-3xl italic">PokéMath</div>
         </Link>
       </nav>
-
-      <div className="h-36 flex justify-center justify-center items-center">
-        {false && (
-          <p className="text-lg max-w-72 bg-[#ffa500] border-2 border-black border-solid p-2">
-            Wrong answer.
-          </p>
-        )}
-      </div>
+      {showQuestion && (
+        <div className="h-36 flex justify-center justify-center items-center">
+          {false && (
+            <p className="text-lg max-w-72 bg-[#ffa500] border-2 border-black border-solid p-2">
+              Wrong answer.
+            </p>
+          )}
+        </div>
+      )}
 
       <div className="px-2 md:px-8">
-        <Form method="post" className="px-4 max-w-sm mx-auto space-y-4 md:px-8">
-          <p className="text-center text-xl mb-4">1 + 1 =</p>
-          <input
-            name="answer"
-            placeholder="answer"
-            className="w-full p-2 border border-gray-400"
-          />
-          <div className="mt-8 space-x-6 text-right">
-            <button
-              type="submit"
-              className="bg-black px-4 py-2 text-xl font-medium text-white hover:bg-gray-600 w-full"
+        {showQuestion && (
+          <Form
+            method="post"
+            className="px-4 max-w-sm mx-auto space-y-4 md:px-8"
+          >
+            <p className="text-center text-xl mb-4">1 + 1 =</p>
+            <input
+              name="answer"
+              placeholder="answer"
+              className="w-full p-2 border border-gray-400"
+            />
+            <div className="mt-8 space-x-6 text-right">
+              <button
+                type="button"
+                onClick={handleSubmit}
+                className="bg-black px-4 py-2 text-xl font-medium text-white hover:bg-gray-600 w-full"
+              >
+                submit
+              </button>
+            </div>
+          </Form>
+        )}
+        {showGotAwayMessage && (
+          <div className="md:mt-16 px-2 md:px-8">
+            <div className="h-56 mb-4  md:h-64 flex justify-center justify-center items-center">
+              <p className="text-lg max-w-72 bg-white border-2 border-black border-solid p-2">
+                The pokemon got away.
+              </p>
+            </div>
+            <Form
+              method="post"
+              className="px-4 max-w-sm mx-auto space-y-4 md:px-8"
             >
-              submit
-            </button>
+              <button
+                type="button"
+                onClick={handleOkClick}
+                className="bg-black px-4 py-2 text-xl font-medium text-white hover:bg-gray-600 w-full"
+              >
+                ok
+              </button>
+            </Form>
           </div>
-        </Form>
+        )}
+        {showNewPokemon && (
+          <div className="md:mt-16 px-2 md:px-8">
+            <img
+              className={`w-56 h-56 mb-4 md:w-64 md:h-64 mx-auto`}
+              src={`https://pokemons.pages.dev/sprites/pm0001_00_00_00_big.png`}
+            />
+            <Form
+              method="post"
+              className="px-4 max-w-sm mx-auto space-y-4 md:px-8"
+            >
+              <button
+                type="button"
+                onClick={handleOptionClick}
+                className="bg-black px-4 py-2 text-xl font-medium text-white hover:bg-gray-600 w-full"
+              >
+                keep
+              </button>
+              <button
+                type="button"
+                onClick={handleOptionClick}
+                className="bg-black px-4 py-2 text-xl font-medium text-white hover:bg-gray-600 w-full"
+              >
+                release
+              </button>
+            </Form>
+          </div>
+        )}
       </div>
     </>
   );
