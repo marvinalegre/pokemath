c3461ca8265e5c2924a2f76f581e6ebab69b4704
Marvin Alegre <marvinalegredev@gmail.com>
2024-12-28 15:07:01 +0800

    rewrite login post endpoint

--- apps/api/app.js
+++ apps/api/app.js
@@ -109,4 +109,42 @@ app.post("/api/signup", async (c) => {
   return c.json({ success: true });
 });
 
+app.post("/api/login", async (c) => {
+  const { username, password } = await c.req.json();
+
+  try {
+    validateUsername(username);
+  } catch (e) {
+    return c.json({ err: e.message });
+  }
+
+  const { results: users } = await c.env.DB.prepare(
+    "select username, hashed_password, jwt_id from users where username = ?"
+  )
+    .bind(username)
+    .all();
+
+  if (!users.length) return c.json({ err: "Incorrect username or password." });
+
+  try {
+    validatePassword(password);
+  } catch (e) {
+    return c.json({ err: e.message });
+  }
+
+  if (!(await bcrypt.compare(password, users[0].hashed_password)))
+    return c.json({ err: "Incorrect username or password." });
+
+  const jwtId = users[0].jwt_id;
+  const token = await sign({ jwtId }, c.env.JWT_SECRET);
+  setCookie(c, "token", token, {
+    path: "/",
+    secure: true,
+    httpOnly: true,
+    expires: new Date(Date.now() + 2_592_000_000 * 3),
+    sameSite: "Strict",
+  });
+
+  return c.json({ success: true });
+});
 export default app;
