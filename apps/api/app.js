aa62d9347edb034837c2c4a7cb9d797b9afa5595
Marvin Alegre <marvinalegredev@gmail.com>
2024-12-28 10:12:28 +0800

    rewrite user endpoint

--- apps/api/app.js
+++ apps/api/app.js
@@ -1,14 +1,45 @@
 import { Hono } from "hono";
+import { getCookie, setCookie } from "hono/cookie";
+import { verify, sign } from "hono/jwt";
+
 const app = new Hono();
 
 app.use("*", (c, next) => {
-  console.log(c.req.path); ///
+  // SPA redirect
+
   if (c.req.path.startsWith("/api")) {
     return next();
   }
-  const reqUrl = new URL(c.req.raw.url); ///
-  return c.env.ASSETS.fetch(new URL("/index.html", reqUrl.origin)); ///
+
+  return c.env.ASSETS.fetch(new URL("/index.html", reqUrl.origin));
 });
-app.get("/api/hono", (c) => c.text("Hono!"));
+
+app.get("/api/user", async (c) => {
+  const token = getCookie(c, "token");
+  if (token == undefined) {
+    return c.json({ loggedIn: false });
+  }
+
+  let jwtId;
+  try {
+    const decodedPayload = await verify(token, c.env.JWT_SECRET, "HS256");
+    jwtId = decodedPayload.jwtId;
+  } catch (e) {
+    console.log(e.message);
+    return c.json({ loggedIn: false });
+  }
+
+  const { results: users } = await c.env.DB.prepare(
+    "select username from users where jwt_id = ?"
+  )
+    .bind(jwtId)
+    .all();
+
+  if (users.length === 1) {
+    return c.json({ loggedIn: true, username: users[0].username });
+  } else {
+    return c.json({ loggedIn: false });
+  }
+});
 
 export default app;
