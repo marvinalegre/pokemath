5d74ef9c572d1913941f0c72386a9cac48698694
Marvin Alegre <marvinalegredev@gmail.com>
2024-12-28 12:59:02 +0800

    update validateUsername function

--- packages/validation/index.js
+++ packages/validation/index.js
@@ -1,12 +1,25 @@
 export function validateUsername(username) {
-  if (username === "null") return "Please enter a username.";
-  if (username.length < 3)
-    return "The username must contain a minimum of 3 characters.";
-  if (username.length > 20)
-    return "The username must contain a maximum of 20 characters.";
-  if (/^\d/.test(username)) return "The username cannot begin with a number.";
-  if (!/^[a-z0-9]+$/.test(username))
-    return "The username may only contain letters and numbers.";
+  class ValidationError extends Error {}
 
-  return "Username is valid.";
+  if (typeof username !== "string") {
+    throw new ValidationError("Username is not valid.");
+  }
+  if (username.length < 3) {
+    throw new ValidationError(
+      "The username must contain a minimum of 3 characters."
+    );
+  }
+  if (username.length > 20) {
+    throw new ValidationError(
+      "The username must contain a maximum of 20 characters."
+    );
+  }
+  if (/^\d/.test(username)) {
+    throw new ValidationError("The username cannot begin with a number.");
+  }
+  if (!/^[a-z0-9]+$/.test(username)) {
+    throw new ValidationError(
+      "The username may only contain letters and numbers."
+    );
+  }
 }
