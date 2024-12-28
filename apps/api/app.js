008b8f9430055eca85666a1d36a081ca1278f956
Marvin Alegre <marvinalegredev@gmail.com>
2024-12-28 14:28:59 +0800

    rewrite signup post endpoint

--- apps/api/app.js
+++ apps/api/app.js
@@ -1,6 +1,10 @@
 import { Hono } from "hono";
 import { getCookie, setCookie } from "hono/cookie";
 import { verify, sign } from "hono/jwt";
+import zxcvbn from "zxcvbn";
+import bcrypt from "bcryptjs";
+import { nanoid } from "nanoid";
+import { validateUsername, validatePassword } from "@pokemath/validation";
 
 const app = new Hono();
 
@@ -40,6 +44,69 @@ app.get("/api/user", async (c) => {
   } else {
     return c.json({ loggedIn: false });
   }
+});
+
+app.post("/api/signup", async (c) => {
+  const { username, password } = await c.req.json();
+
+  try {
+    validateUsername(username);
+  } catch (e) {
+    return c.json({ err: e.message });
+  }
+
+  const { results: users } = await c.env.DB.prepare(
+    "select username from users where username = ?"
+  )
+    .bind(username)
+    .all();
+
+  if (users.length) {
+    return c.json({ err: "This username is not available." });
+  }
+
+  try {
+    validatePassword(password);
+  } catch (e) {
+    return c.json({ err: e.message });
+  }
+
+  const zxcvbnOutput = zxcvbn(password);
+  if (zxcvbnOutput.score < 3) {
+    let err = "The password is weak.";
+    if (zxcvbnOutput.feedback.warning) {
+      err = `${err} ${zxcvbnOutput.feedback.warning}.`;
+    } else if (zxcvbnOutput.feedback.suggestions.length > 0) {
+      err = `${err} ${zxcvbnOutput.feedback.suggestions[0]}`;
+    }
+    return c.json({ err });
+  }
+
+  const hash = await bcrypt.hash(password, 10);
+  const jwtId = nanoid();
+
+  // TODO: handle collisions on jwt_id
+  try {
+    await c.env.DB.prepare(
+      "insert into users (jwt_id, username, hashed_password) values (?, ?, ?)"
+    )
+      .bind(jwtId, username, hash)
+      .run();
+  } catch (e) {
+    console.log(e.message);
+    return Response.json({ err: "Something went wrong." });
+  }
+
+  const token = await sign({ jwtId }, c.env.JWT_SECRET);
+  setCookie(c, "token", token, {
+    path: "/",
+    secure: true,
+    httpOnly: true,
+    expires: new Date(Date.now() + 2_592_000_000 * 3),
+    sameSite: "Strict",
+  });
+
+  return c.json({ success: true });
 });
 
 export default app;
