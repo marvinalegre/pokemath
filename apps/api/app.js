78f3ca95c0af8ca79aa458c7718ded9ed8457f7c
Marvin Alegre <marvinalegredev@gmail.com>
2024-12-28 20:49:34 +0800

    rewrite pokemon endpoint

--- apps/api/app.js
+++ apps/api/app.js
@@ -206,4 +206,26 @@ app.get("/api/player/:username", async (c) => {
   });
 });
 
+app.get("/api/pokemon/:id", async (c) => {
+  if (!Number.isInteger(Number(c.req.param("id")))) {
+    return c.json({ err: "Invalid input." });
+  }
+
+  const { results } = await c.env.DB.prepare(
+    "select name, description, color from pokemons where id = ?"
+  )
+    .bind(c.req.param("id"))
+    .all();
+
+  if (results.length === 0) {
+    return c.json({ err: "Invalid input." });
+  }
+
+  return c.json({
+    name: results[0].name,
+    description: results[0].description,
+    color: results[0].color,
+  });
+});
+
 export default app;
