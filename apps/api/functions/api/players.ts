e2b939ec44c798d04d3a822bfac8f628d3fb9c7c
Marvin Alegre <marvinalegredev@gmail.com>
2024-12-31 13:41:01 +0800

    remove pages functions code

--- apps/api/functions/api/players.ts
+++
@@ -1,7 +0,0 @@
-export const onRequestGet = async (context) => {
-  const { results } = await context.env.DB.prepare(
-    "select username from users"
-  ).all();
-
-  return Response.json({ players: results });
-};
