82b53d2f2b342713488c54137b47697e405dec4b
Marvin Alegre <marvinalegredev@gmail.com>
2025-09-01 22:37:59 +0800

    add morgan, helmet, compression

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 import express from "express";
 import path from "path";
+import fs from "fs";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import compression from "compression";
+import morgan from "morgan";
 
 import indexRoutes from "./routes/index.js";
 import authRoutes from "./routes/auth.js";
 import catchRoutes from "./routes/catch.js";
 
-const PORT = process.env.PORT || 3000;
-
 const app = express();
+app.use(helmet());
+const accessLogStream = fs.createWriteStream(
+  path.join(process.cwd(), "logs", "access.log"),
+  { flags: "a" },
+);
+app.use(morgan("combined", { stream: accessLogStream }));
+app.use(compression());
+
 app.set("view engine", "ejs");
 app.set("views", path.join(process.cwd(), "views"));
 
@@ -29,4 +39,5 @@ app.use((err, _req, res, _next) => {
   res.status(500).send("Internal server error");
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Now listening at ${PORT}.`));
