b2c31271a22d97af2369f346e9b2e4b8e042dc4a
Marvin Alegre <marvinalegredev@gmail.com>
2025-09-11 08:29:52 +0800

    add new pokemons

---
+++ database/migrations/005_add_seven_pokemons.sql
@@ -0,0 +1,7 @@
+INSERT INTO pokemons VALUES(16,'Pidgey', 'A common sight in forests and woods. It flaps its wings at ground level to kick up blinding sand.' ,'#e5d5bf','C', 17, 51);
+INSERT INTO pokemons VALUES(17,'Pidgeotto', 'Very protective of its sprawling territorial area, this POKéMON will fiercely peck at any intruder.' ,'#e7d8dc','CE', 18, 152);
+INSERT INTO pokemons VALUES(18,'Pidgeot','When hunting, it skims the surface of water at high speed to pick off unwary prey such as MAGIKARP.','#f8f3e2','CEE', NULL, NULL);
+INSERT INTO pokemons VALUES(19,'Rattata', 'Bites anything when it attacks. Small and very quick, it is a common sight in many places.' ,'#d8ccdd','C', 20, 51);
+INSERT INTO pokemons VALUES(20,'Raticate', 'It uses its whiskers to maintain its balance. It apparently slows down if they are cut off.' ,'#d3c2b0','CE', NULL, NULL);
+INSERT INTO pokemons VALUES(21,'Spearow', 'Eats bugs in grassy areas. It has to flap its short wings at high speed to stay airborne.' ,'#edc5c3','C', 22, 51);
+INSERT INTO pokemons VALUES(22,'Fearow', 'With its huge and magnificent wings, it can keep aloft without ever having to land for rest.' ,'#dcc8b8','CE', NULL, NULL);
