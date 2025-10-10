b1ce0e9e4e44a920e38ff8540671f79f4e9e1487
Marvin Alegre <marvinalegredev@gmail.com>
2025-10-10 14:11:36 +0800

    add 979

---
+++ database/migrations/007_add_979.sql
@@ -0,0 +1,3 @@
+INSERT INTO pokemons VALUES(979,'Annihilape', 'When its anger rose beyond a critical point, this Pokémon gained power that is unfettered by the limits of its physical body.' ,'#c5c7ca','TEE', NULL, NULL);
+INSERT INTO pokemon_types VALUES(979, 2);
+INSERT INTO pokemon_types VALUES(979, 8);
