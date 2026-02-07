a3035ea459967cef76d64e9ab8f85ce2996c0e3d
Marvin Alegre <marvinalegredev@gmail.com>
2026-02-07 13:13:10 +0800

    add DATABASE_URL env var

--- database/db.js
+++ database/db.js
@@ -1,5 +1,5 @@
 import Database from "better-sqlite3";
-const db = new Database("./database/dev.db");
+const db = new Database(process.env.DATABASE_URL);
 db.pragma("journal_mode = WAL");
 
 export default db;
