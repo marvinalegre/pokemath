9bb845c101056ee9814feea0170aa0cb64785872
Marvin Alegre <marvinalegredev@gmail.com>
2026-02-20 22:20:18 +0800

    connect api and web in dev

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetch("http://api:3000");
+  const { message } = await res.json();
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -13,6 +16,7 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <p>message from nest: {message}</p>
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
           </h1>
