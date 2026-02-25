07485c4e3dc6a56976180b54197aee736523c74f
Marvin Alegre <marvinalegredev@gmail.com>
2026-02-25 09:47:06 +0800

    include components to watch paths

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,5 +1,6 @@
 import { ComponentExample } from "@/components/component-example";
 import Message from "./message";
+import Test from "@/components/test";
 
 export default async function Page() {
   const res = await fetch("http://pokemath-api:3000");
@@ -9,6 +10,7 @@ export default async function Page() {
     <>
       <p>message from nest (via node rsc server): {message}</p>
       <Message />
+      <Test />
       <ComponentExample />
     </>
   );
