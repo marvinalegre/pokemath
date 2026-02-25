545880747dc40daab586a769b3abfda337d8b46c
Marvin Alegre <marvinalegredev@gmail.com>
2026-02-25 13:32:04 +0800

    tweak next config

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,6 +2,8 @@ import Navbar from "@/components/navbar";
 import Message from "./message";
 import Test from "@/components/test";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
   const res = await fetch("http://pokemath-api:3000");
   const { message } = await res.json();
