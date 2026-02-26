6ab3b2c10590fafaf956f459196deaf1fdffb9f4
Marvin Alegre <marvinalegredev@gmail.com>
2026-02-26 13:54:13 +0800

    add global error handler

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +24,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  throw new Error("foobar");
+
   return (
     <html lang="en" className={inter.variable}>
       <body
