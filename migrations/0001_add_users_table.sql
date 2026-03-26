86e105c54a56ccfcd8fbdedab806568dfcee9514
Marvin Alegre <marvinalegredev@gmail.com>
2026-03-26 10:08:41 +0800

    remove jwt_sub from users table

--- migrations/0001_add_users_table.sql
+++ migrations/0001_add_users_table.sql
@@ -1,14 +1,7 @@
 -- Migration number: 0001 	 2026-03-24T12:21:20.786Z
-CREATE TABLE `users` (
-  `id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-  `username` text NOT NULL,
-  `jwt_sub` text NOT NULL,
-  `created_at` text DEFAULT CURRENT_TIMESTAMP,
-  `role` text DEFAULT 'guest' NOT NULL
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  username TEXT NOT NULL UNIQUE,
+  created_at TEXT DEFAULT CURRENT_TIMESTAMP,
+  role TEXT DEFAULT 'guest' NOT NULL
 );
-
---> statement-breakpoint
-CREATE UNIQUE INDEX `users_username_unique` ON `users` (`username`);
-
---> statement-breakpoint
-CREATE UNIQUE INDEX `users_jwt_sub_unique` ON `users` (`jwt_sub`);
