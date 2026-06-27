752f8712a3eab00057e0c3593d1425f87150c498
Marvin Alegre <marvinalegredev@gmail.com>
2026-06-27 21:07:50 +0800

    server render katex

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,17 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import katex from 'katex';
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		const url = new URL(request.url);
-		switch (url.pathname) {
-			case '/message':
-				return new Response('Hello, World!');
-			case '/random':
-				return new Response(crypto.randomUUID());
-			default:
-				return new Response('Not Found', { status: 404 });
-		}
-	},
+    async fetch(request, env, ctx): Promise<Response> {
+        const url = new URL(request.url);
+        switch (url.pathname.slice(4)) {
+            case '/active-question':
+                return new Response(`${katex.renderToString('10 + 10 = \\text{?}')}`, {
+                    headers: { 'Content-Type': 'text/html' },
+                });
+            case '/random':
+                return new Response(crypto.randomUUID());
+            default:
+                return new Response('Not Found', { status: 404 });
+        }
+    },
 } satisfies ExportedHandler<Env>;
